008c1cb3536a2b84d3ac6fda089eaef68b5cadd3
Nettan <jeanette.norberg82@gmail.com>
2024-10-22 10:37:09 +0200

    Added player/game class and functions

---
+++ Minesweep/src/Game.java
@@ -0,0 +1,36 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Game {
+
+    int winn = 0;
+    int lost = 0;
+
+    public void game(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to MineSweeper!");
+
+        System.out.println("Print your name: ");
+        String userName = scanner.nextLine();
+        Player player = new Player(userName);
+        System.out.println(player.getName());
+    }
+
+    public Game() {
+    }
+
+    public void totalScore(){
+        // if(user winns) winn ++
+        winn = getWinn() + 1;
+        // else if (user lost) lost ++
+    }
+
+
+    public int getWinn() {
+        return winn;
+    }
+
+    public int getLost() {
+        return lost;
+    }
+}
