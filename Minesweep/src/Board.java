abdb1c3dc2a7eed94e04b0113a8ff800c15d0398
camillafalk <ccamilla.falk@gmail.com>
2024-10-22 10:52:57 +0200

    added printBoard()

--- Minesweep/src/Board.java
+++ Minesweep/src/Board.java
@@ -1,2 +1,37 @@
 public class Board {
+
+    char [][] board;
+    int row;
+    int column;
+    int mines;
+
+    public Board(int row, int column, int mines) {
+        this.board = new char[7][7];
+        this.row = row;
+        this.column = column;
+        this.mines = mines;
+    }
+
+    public void printBoard() {
+
+        for (char letter = 'A'; letter < 'G'; letter++) {
+            System.out.print("     " + letter);
+        }
+        System.out.println();
+        System.out.println("---+-----+-----+-----+-----+-----+-----+");
+
+
+        for (int r = 1; r < 7; r++) {
+            System.out.print(r);
+            for (int c = 0; c < 6; c++) {
+
+                    System.out.print("  |  " + board[r][c]);
+                if (c == 5) {
+                    System.out.println("  | ");
+                }
+            }
+            System.out.println("---+-----+-----+-----+-----+-----+-----+");
+        }
+    }
+
 }
--- Minesweep/src/Main.java
+++ Minesweep/src/Main.java
@@ -2,6 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Board board = new Board(7, 7, 6);
+
+        board.printBoard();
+
+
+
     }
 }
