b577c3d6b89a1fce0e737cd135f19371e66c3020
emiliaforsheim <emiliaforsheim@outlook.com>
2024-10-23 09:12:37 +0200

    second commit

--- Minesweep/src/Board.java
+++ Minesweep/src/Board.java
@@ -48,7 +48,7 @@ public class Board {
                     System.out.println("  | ");
                 }
             }
-            System.out.print("----");
+            System.out.print("---");
             for (int c = 0; c < column; c++) {
                 System.out.print("+-----");
             }
