57fffd99c20f4b4e578da300307242d4c7ee0408
camillafalk <ccamilla.falk@gmail.com>
2024-10-23 09:57:19 +0200

    Fixed a try/catch in Game-class

--- Minesweep/src/Game.java
+++ Minesweep/src/Game.java
@@ -1,4 +1,5 @@
 import java.sql.SQLOutput;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Game {
@@ -14,52 +15,58 @@ public class Game {
 
 
         while (true) {//checks is input is either 1 or 2, crashes if input is anythinig but number, need try catch
-            rulesOrGame = scanner.nextInt();
-            if (rulesOrGame == 2) {
-                scanner.nextLine();
-                System.out.println("Print your name: ");
-                String userName = scanner.nextLine();
-                Player player = new Player(userName);
-                System.out.println(player.getName());
-                System.out.println("Please choose difficulty level: ");
-                System.out.println("1. Easy");
-                System.out.println("2. Medium");
-                System.out.println("3. Hard");
-                int a;
-                while (true) { // loops until user choose 1, 2 or 3, crashes if input is not a number, need try catch
-                    a = scanner.nextInt();
-                    if (a == 1 || a == 2 || a == 3) {
-                        switch (a) {
-                            case 1:
-                                Board easyBoard = new Board(7, 7, 6);
-                                easyBoard.printBoard();
+            try {
+                rulesOrGame = scanner.nextInt();
+                if (rulesOrGame == 2) {
+                    scanner.nextLine();
+                    System.out.println("Print your name: ");
+                    String userName = scanner.nextLine();
+                    Player player = new Player(userName);
+                    System.out.println(player.getName());
+                    System.out.println("Please choose difficulty level: ");
+                    System.out.println("1. Easy");
+                    System.out.println("2. Medium");
+                    System.out.println("3. Hard");
+                    int a;
+                    while (true) { // loops until user choose 1, 2 or 3, crashes if input is not a number, need try catch
+                        try {
+                            a = scanner.nextInt();
+                            if (a == 1 || a == 2 || a == 3) {
+                                switch (a) {
+                                    case 1:
+                                        Board easyBoard = new Board(7, 7, 6);
+                                        easyBoard.printBoard();
+                                        break;
+                                    case 2:
+                                        Board mediumBoard = new Board(12, 12, 10);
+                                        mediumBoard.printBoard();
+                                        break;
+                                    case 3:
+                                        Board hardBoard = new Board(15, 15, 12);
+                                        hardBoard.printBoard();
+                                        break;
+                                }
                                 break;
-                            case 2:
-                                Board mediumBoard = new Board(12, 12, 10);
-                                mediumBoard.printBoard();
-                                break;
-                            case 3:
-                                Board hardBoard = new Board(15, 15, 12);
-                                hardBoard.printBoard();
-                                break;
-                            default:
-                                System.out.println("Invalid choice");
-                                break;
-                        }break;
-                    } else {
-                        System.out.println("Invalid input, try again");
+                            } else {
+                                System.out.println("Invalid input, try again");
+                            }
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid choice");
+                            scanner.nextLine();
+                        }
                     }
+                } else if (rulesOrGame == 1) {
+                    System.out.println("rules");
+                    break;
+                } else {
+                    System.out.println("Invalid input");
 
                 }
-            } else if (rulesOrGame == 1) {
-                System.out.println("rules");
-                break;
-            } else {
+            } catch (InputMismatchException e) {
                 System.out.println("Invalid input");
+                scanner.nextLine();
             }
         }
-
-       
     }
 
     public Game() {
