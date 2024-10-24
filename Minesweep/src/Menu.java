07d9001c5d607a8295aefe414634226e676f9e3a
camillafalk <ccamilla.falk@gmail.com>
2024-10-24 09:18:58 +0200

    Ignore this commit

--- Minesweep/src/Menu.java
+++ Minesweep/src/Menu.java
@@ -11,7 +11,6 @@ public class Menu {
      */
     public void menu() {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Please press 1 for rules or press 2 to start the game");
         int rulesOrGame;
 
